faed040aa164e40ce00c0241b32603ea7b4cab7c
Ruby Pink <hello@rubypinkromance.com>
2025-12-04 20:59:39 -0800

    add stylelint

---
+++ .stylelintrc.js
@@ -0,0 +1,8 @@
+export default {
+	extends: ['stylelint-config-spaceninja'],
+	rules: {
+		'plugin/no-low-performance-animation-properties': null,
+		'selector-no-qualifying-type': null,
+		'selector-max-id': null,
+	},
+};
